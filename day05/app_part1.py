8e65135509bc01c34669bd6c8bd753916c329d93
Dan Johns <danjhd@amazon.co.uk>
2021-12-05 09:33:02 +0000

    remove numpy add regex

--- day05/app_part1.py
+++ day05/app_part1.py
@@ -1,25 +1,14 @@
-import numpy as np
-
-def parse_lines(data):
-    dicts = []
-    for line in data:
-        split_line = line.split('->')
-        x1, y1 = split_line[0].strip().split(',')
-        x2, y2 = split_line[1].strip().split(',')
-        dicts.append({'x1': int(x1), 'y1': int(y1), 'x2': int(x2), 'y2': int(y2),})
-    return dicts
+import re
 
 if __name__ == '__main__':
 #    file = open('sample', 'r')
     file = open('input', 'r')
-    lines = file.readlines()
-    lines = [l.strip() for l in lines]
-
-    lines = parse_lines(lines)
+    lines = [m.groupdict() for m in re.finditer(r'(?P<x1>\d+),(?P<y1>\d+)\s->\s(?P<x2>\d+),(?P<y2>\d+)', file.read())]
+    lines = [{'x1': int(l['x1']), 'y1': int(l['y1']), 'x2': int(l['x2']), 'y2': int(l['y2'])} for l in lines]
 
     max_x = max([x for a in [[l['x1'], l['x2']] for l in lines] for x in a])
     max_y = max([y for a in [[l['y1'], l['y2']] for l in lines] for y in a])
-    coords = np.zeros([max_y + 1, max_x + 1])
+    coords = [[0 for x in range(max_x + 1)] for y in range(max_y + 1)]
 
     for s in lines:
         if s['x1'] == s['x2']:

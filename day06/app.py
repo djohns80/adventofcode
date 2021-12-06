308bd3484f2507133d9b943253507b3722cd048e
Dan Johns <danjhd@amazon.co.uk>
2021-12-06 10:25:32 +0000

    day 6

---
+++ day06/app.py
@@ -0,0 +1,20 @@
+if __name__ == '__main__':
+#    file = open('sample', 'r')
+    file = open('input', 'r')
+    input = file.read()
+
+    lantern_fish = [int(i) for i in input.split(',')]
+    fish_counts = [0]*9
+
+    for fish in lantern_fish:
+        fish_counts[fish] += 1
+
+    for d in range(256):
+        new_fish = fish_counts[0]
+        for i in range(len(fish_counts)-1):
+            fish_counts[i] = fish_counts[i+1]
+        fish_counts[8] = new_fish
+        fish_counts[6] += new_fish
+    
+    print(sum(fish_counts))
+

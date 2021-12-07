17080f2d2ab85d433884aef5e1f8d77361c19bcd
Dan Johns <danjhd@amazon.co.uk>
2021-12-07 07:21:36 +0000

    clarifying code

--- day06/app.py
+++ day06/app.py
@@ -9,12 +9,24 @@ if __name__ == '__main__':
     for fish in lantern_fish:
         fish_counts[fish] += 1
 
+###########
+## part 1 #
+###########
+    for d in range(80):
+        new_fish = fish_counts[0]
+        for i in range(len(fish_counts)-1):
+            fish_counts[i] = fish_counts[i+1]
+        fish_counts[8] = new_fish
+        fish_counts[6] += new_fish
+    print(sum(fish_counts))
+
+###########
+## part 2 #
+###########
     for d in range(256):
         new_fish = fish_counts[0]
         for i in range(len(fish_counts)-1):
             fish_counts[i] = fish_counts[i+1]
         fish_counts[8] = new_fish
         fish_counts[6] += new_fish
-    
     print(sum(fish_counts))
-

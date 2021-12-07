3835ad213c9e1da403821f01caf1cf5d215a7efd
Dan Johns <danjhd@amazon.co.uk>
2021-12-07 09:07:27 +0000

    standardising layout

--- 2021/day01/app.py
+++ 2021/day01/app.py
@@ -1,28 +1,29 @@
-file = open('input', 'r')
-Lines = file.readlines()
+if __name__ == '__main__':
+    file = open('input', 'r')
+    Lines = file.readlines()
 
 ##########
 # part 1 #
 ##########
-previous_value = None
-increase_count = 0
-for line in Lines:
-    current_value = int(line.strip())
-    if previous_value is not None:
-        if current_value > previous_value:
-            increase_count += 1
-    previous_value = current_value
-print(increase_count)
+    previous_value = None
+    increase_count = 0
+    for line in Lines:
+        current_value = int(line.strip())
+        if previous_value is not None:
+            if current_value > previous_value:
+                increase_count += 1
+        previous_value = current_value
+    print(increase_count)
 
 ##########
 # part 2 #
 ##########
-previous_value = None
-increase_count = 0
-for n in range(2, len(Lines)):
-    current_value = int(Lines[n-2].strip()) + int(Lines[n-1].strip()) + int(Lines[n].strip())
-    if previous_value is not None:
-        if current_value > previous_value:
-            increase_count += 1
-    previous_value = current_value
-print(increase_count)
+    previous_value = None
+    increase_count = 0
+    for n in range(2, len(Lines)):
+        current_value = int(Lines[n-2].strip()) + int(Lines[n-1].strip()) + int(Lines[n].strip())
+        if previous_value is not None:
+            if current_value > previous_value:
+                increase_count += 1
+        previous_value = current_value
+    print(increase_count)
--- 2021/day03/app_part1.py
+++ 2021/day03/app_part1.py
@@ -1,16 +1,17 @@
-#file = open('sample', 'r')
-file = open('input', 'r')
-lines = file.readlines()
-lines = [l.strip() for l in lines]
+if __name__ == '__main__':
+    #file = open('sample', 'r')
+    file = open('input', 'r')
+    lines = file.readlines()
+    lines = [l.strip() for l in lines]
 
-line_count = len(lines)
-common = []
-for line in lines:
-    for n, b in enumerate(line):
-        if len(common) <= n:
-            common.append(int(b))
-        else:
-            common[n] += int(b)
-gamma = int(''.join([str(int(b > (line_count / 2))) for b in common]), 2)
-epsilon = int(''.join([str(int(not b > (line_count / 2))) for b in common]), 2)
-print(gamma * epsilon)
+    line_count = len(lines)
+    common = []
+    for line in lines:
+        for n, b in enumerate(line):
+            if len(common) <= n:
+                common.append(int(b))
+            else:
+                common[n] += int(b)
+    gamma = int(''.join([str(int(b > (line_count / 2))) for b in common]), 2)
+    epsilon = int(''.join([str(int(not b > (line_count / 2))) for b in common]), 2)
+    print(gamma * epsilon)

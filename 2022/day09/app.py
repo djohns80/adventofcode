a431d03e885314fa0c02f868c32dc4d2f3c4df84
Dan Johns <danjhd@amazon.com>
2022-12-09 08:02:17 +0000

    2022 day 9

--- 2022/day09/app.py
+++ 2022/day09/app.py
@@ -3,40 +3,9 @@ import math
 import os
 from collections import defaultdict
 
-def main(input_file):
-    file = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), input_file), 'r', encoding='utf-8')
-    motions = [m.split() for m in file.read().strip().split('\n')]
-
+def tail_path_len(motions, knots):
     movements = {'L': -1 + 0j, 'R': 1 + 0j, 'U': 0 + 1j, 'D': 0 - 1j}
-
-##########
-# part 1 #
-##########
-    h_pos = 0 + 0j
-    t_pos = 0 + 0j
-    t_visits = defaultdict(int)
-    t_visits[t_pos] += 1
-    for m in motions:
-        for _ in range(int(m[1])):
-            h_pos += movements[m[0]]
-            while abs(h_pos - t_pos) > math.sqrt(2):
-                correction = 0 + 0j
-                if (h_pos - t_pos).real > 0:
-                    correction += movements['R']
-                elif (h_pos - t_pos).real < 0:
-                    correction += movements['L']
-                if (h_pos - t_pos).imag > 0:
-                    correction += movements['U']
-                elif (h_pos - t_pos).imag < 0:
-                    correction += movements['D']
-                t_pos += correction
-            t_visits[t_pos] += 1
-    print(len(t_visits))
-
-##########
-# part 2 #
-##########
-    knots = [0 + 0j] * 10
+    knots = [0 + 0j] * knots
     t_visits = defaultdict(int)
     t_visits[knots[-1]] += 1
     for m in motions:
@@ -55,7 +24,21 @@ def main(input_file):
                         correction += movements['D']
                     knots[k+1] += correction
             t_visits[knots[-1]] += 1
-    print(len(t_visits))
+    return len(t_visits)
+
+def main(input_file):
+    file = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), input_file), 'r', encoding='utf-8')
+    data = [m.split() for m in file.read().strip().split('\n')]
+
+##########
+# part 1 #
+##########
+    print(tail_path_len(data, 2))
+
+##########
+# part 2 #
+##########
+    print(tail_path_len(data, 10))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
